c4a8b1ea8d7bb1659f25fe02ffe01a9bfd329118
Piyush <piyushgargofficial@gmail.com>
2021-04-30 17:08:18 +0530

    Widgets Section Completed

--- src/Components/Widgets.js
+++ src/Components/Widgets.js
@@ -1,10 +1,38 @@
 import React from "react";
 import "../CSS/Widgets.css";
+import {
+  TwitterTimelineEmbed,
+  TwitterShareButton,
+  TwitterTweetEmbed,
+} from "react-twitter-embed";
+import SearchIcon from "@material-ui/icons/Search";
 
 function Widgets() {
   return (
     <div className="widgets">
-      <h2>Widgets</h2>
+      <div className="widgets__input">
+        <SearchIcon className="widgets__searchIcon" />
+        <input placeholder="Search Twitter" type="text" />
+      </div>
+
+      <div className="widgets__widgetContainer">
+        <h2>What's happening</h2>
+
+        {/* Brings the tweet and all the information using the tweet id with the styling as well. It's like an embedding*/}
+        <TwitterTweetEmbed tweetId={"1387901003664699392"} />
+
+        {/* Brings the whole timeline based on the screenName */}
+        <TwitterTimelineEmbed
+          sourceType="profile"
+          screenName="elonmusk"
+          options={{ height: 400 }}
+        />
+        {/* Takes you to twitter where all the text mentioned in the options appear */}
+        <TwitterShareButton
+          url={"https://facebook.com/piyushgarg01"}
+          options={{ text: "#reactjs is awesome", via: "Piyushxx" }}
+        />
+      </div>
     </div>
   );
 }
